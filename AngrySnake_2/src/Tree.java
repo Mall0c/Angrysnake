0f91620d18b616b6db8cebeb3c6eabe4fb6e8517
Richard Steinbrecht <richardsteinbrecht@hotmail.de>
2017-12-08 18:55:54 +0100

    TreeNode und Tree hochgeladen

---
+++ AngrySnake_2/src/Tree.java
@@ -0,0 +1,29 @@
+
+public class Tree {
+	TreeNode root;
+	Spielfeld gameField;
+	
+	public Tree(int x1, int y1,int value, Spielfeld spielfeld) {
+		this(x1, y1, 0, 0, value, spielfeld);
+	}
+	
+	public Tree(int x1, int y1, int x2, int y2, int value, Spielfeld spielfeld) {
+		this.gameField = spielfeld;
+		root = new TreeNode(x1, y1, x2, y2, value);
+	}
+	
+	public void generateTree(Tree t) {
+		gameField.
+		if(this.gameField[(xKopf-1+7)%7][yKopf] == ' ') {
+			if(this.gameField[(xKopf-2+7)%7][yKopf] == ' ') { 					// nach links
+				return true;
+			} else if(this.gameField[(xKopf-1+7)%7][(yKopf-1+7)%7] == ' ') {	// nach unten
+				return true;
+			} else if(this.gameField[(xKopf-1+7)%7][(yKopf+1+7)%7] == ' ') {	// nach oben
+				return true;
+			}
+		}
+			
+	}
+	
+}
---
+++ AngrySnake_2/src/TreeNode.java
@@ -0,0 +1,184 @@
+
+public class TreeNode {
+	private int x1;
+	private int y1;
+	private int x2;
+	private int y2;
+	private int value;
+	private TreeNode t01;
+	private TreeNode t02;
+	private TreeNode t03;
+	private TreeNode t04;
+	private TreeNode t05;
+	private TreeNode t06;
+	private TreeNode t07;
+	private TreeNode t08;
+	private TreeNode t09;
+	
+	public TreeNode(int x1, int y1, int x2, int y2, int value) {
+		this.setX1(x1);
+		this.setY1(y1);
+		this.setX2(x2);
+		this.setY2(y2);
+		this.setValue(value);
+		
+		this.setT01(null);
+		this.setT02(null);
+		this.setT03(null);
+		this.setT04(null);
+		this.setT05(null);
+		this.setT06(null);
+		this.setT07(null);
+		this.setT08(null);
+		this.setT09(null);
+	}
+	
+	
+	
+	
+	
+	public TreeNode getT01() {
+		return t01;
+	}
+
+	public void setT01(TreeNode t01) {
+		this.t01 = t01;
+	}
+
+	public TreeNode getT02() {
+		return t02;
+	}
+
+	public void setT02(TreeNode t02) {
+		this.t02 = t02;
+	}
+
+	public TreeNode getT03() {
+		return t03;
+	}
+
+	public void setT03(TreeNode t03) {
+		this.t03 = t03;
+	}
+
+	public TreeNode getT04() {
+		return t04;
+	}
+
+	public void setT04(TreeNode t04) {
+		this.t04 = t04;
+	}
+
+	public TreeNode getT05() {
+		return t05;
+	}
+
+	public void setT05(TreeNode t05) {
+		this.t05 = t05;
+	}
+
+	public TreeNode getT06() {
+		return t06;
+	}
+
+	public void setT06(TreeNode t06) {
+		this.t06 = t06;
+	}
+
+	public TreeNode getT07() {
+		return t07;
+	}
+
+	public void setT07(TreeNode t07) {
+		this.t07 = t07;
+	}
+
+	public TreeNode getT08() {
+		return t08;
+	}
+
+	public void setT08(TreeNode t08) {
+		this.t08 = t08;
+	}
+
+	public TreeNode getT09() {
+		return t09;
+	}
+
+	public void setT09(TreeNode t09) {
+		this.t09 = t09;
+	}
+
+
+	public int getX1() {
+		return x1;
+	}
+
+
+	public void setX1(int x1) {
+		this.x1 = x1;
+	}
+
+
+
+
+
+	public int getY1() {
+		return y1;
+	}
+
+
+
+
+
+	public void setY1(int y1) {
+		this.y1 = y1;
+	}
+
+
+
+
+
+	public int getX2() {
+		return x2;
+	}
+
+
+
+
+
+	public void setX2(int x2) {
+		this.x2 = x2;
+	}
+
+
+
+
+
+	public int getY2() {
+		return y2;
+	}
+
+
+
+
+
+	public void setY2(int y2) {
+		this.y2 = y2;
+	}
+
+
+
+
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	
+
+}
