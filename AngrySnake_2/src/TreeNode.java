c2ef10c5569dbdbdb443f47dcb01679ab4a13c5a
assoron <attackspam@web.de>
2017-12-09 15:04:31 +0100

    Comitted 09/12 15:04 changed TreeNode to be the Tree by itself. Rewrote the data structure.

--- AngrySnake_2/src/TreeNode.java
+++ AngrySnake_2/src/TreeNode.java
@@ -1,184 +1,40 @@
+import java.util.ArrayList;
 
-public class TreeNode {
-	private int x1;
-	private int y1;
-	private int x2;
-	private int y2;
-	private int value;
-	private TreeNode t01;
-	private TreeNode t02;
-	private TreeNode t03;
-	private TreeNode t04;
-	private TreeNode t05;
-	private TreeNode t06;
-	private TreeNode t07;
-	private TreeNode t08;
-	private TreeNode t09;
+public class TreeNode<T> {
+
+	public T data;
+	public TreeNode<T> parent;
+	public ArrayList<TreeNode<T>> children;
+
+	public TreeNode(T data) {
+		this.data = data;
+		this.children = new ArrayList<TreeNode<T>>(9);
+	}
 	
-	public TreeNode(int x1, int y1, int x2, int y2, int value) {
-		this.setX1(x1);
-		this.setY1(y1);
-		this.setX2(x2);
-		this.setY2(y2);
-		this.setValue(value);
+	public TreeNode<T> addChild(T child) {
+		TreeNode<T> childNode = new TreeNode<T>(child);
+		childNode.parent = this;
+		this.children.add(childNode);
+		return childNode;
+	}
+	
+	private TreeNode<T> generateTree() {
 		
-		this.setT01(null);
-		this.setT02(null);
-		this.setT03(null);
-		this.setT04(null);
-		this.setT05(null);
-		this.setT06(null);
-		this.setT07(null);
-		this.setT08(null);
-		this.setT09(null);
 	}
 	
+	public boolean isRoot() {
+		return parent == null;
+	}
+
+	public boolean isLeaf() {
+		return children.size() == 0;
+	}
 	
-	
-	
-	
-	public TreeNode getT01() {
-		return t01;
+	public int getLevel() {
+		if (this.isRoot()) {
+			return 0;
+		} else {
+			return parent.getLevel() + 1;
+		}
 	}
-
-	public void setT01(TreeNode t01) {
-		this.t01 = t01;
-	}
-
-	public TreeNode getT02() {
-		return t02;
-	}
-
-	public void setT02(TreeNode t02) {
-		this.t02 = t02;
-	}
-
-	public TreeNode getT03() {
-		return t03;
-	}
-
-	public void setT03(TreeNode t03) {
-		this.t03 = t03;
-	}
-
-	public TreeNode getT04() {
-		return t04;
-	}
-
-	public void setT04(TreeNode t04) {
-		this.t04 = t04;
-	}
-
-	public TreeNode getT05() {
-		return t05;
-	}
-
-	public void setT05(TreeNode t05) {
-		this.t05 = t05;
-	}
-
-	public TreeNode getT06() {
-		return t06;
-	}
-
-	public void setT06(TreeNode t06) {
-		this.t06 = t06;
-	}
-
-	public TreeNode getT07() {
-		return t07;
-	}
-
-	public void setT07(TreeNode t07) {
-		this.t07 = t07;
-	}
-
-	public TreeNode getT08() {
-		return t08;
-	}
-
-	public void setT08(TreeNode t08) {
-		this.t08 = t08;
-	}
-
-	public TreeNode getT09() {
-		return t09;
-	}
-
-	public void setT09(TreeNode t09) {
-		this.t09 = t09;
-	}
-
-
-	public int getX1() {
-		return x1;
-	}
-
-
-	public void setX1(int x1) {
-		this.x1 = x1;
-	}
-
-
-
-
-
-	public int getY1() {
-		return y1;
-	}
-
-
-
-
-
-	public void setY1(int y1) {
-		this.y1 = y1;
-	}
-
-
-
-
-
-	public int getX2() {
-		return x2;
-	}
-
-
-
-
-
-	public void setX2(int x2) {
-		this.x2 = x2;
-	}
-
-
-
-
-
-	public int getY2() {
-		return y2;
-	}
-
-
-
-
-
-	public void setY2(int y2) {
-		this.y2 = y2;
-	}
-
-
-
-
-
-	public int getValue() {
-		return value;
-	}
-
-	public void setValue(int value) {
-		this.value = value;
-	}
-
-	
-
 }
